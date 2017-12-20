6dc04873adff04b203bf09a39e6cbc6c608d9000
Aditya Mhamunkar <me@adityamhamunkar.com>
2017-12-19 20:40:25 -0500

    Update binary-search.py

--- binary-search.py
+++ binary-search.py
@@ -1,4 +1,4 @@
-def binSearch(arr, num, left, right):
+def binSearch(arr, left, right, num):
 	if right >= left:
 		mid = left + (right-left)//2
 		if arr[mid] == num:
@@ -9,9 +9,22 @@ def binSearch(arr, num, left, right):
 			return binSearch(arr, num, mid+1, right)
 	else:
 		return "Not found"
+	
+	
+def binarySearch(arr, begin, end, num):
+    while (begin <= end):
+        mid = begin + (end - begin)//2
+        if (arr[mid] < num):
+            begin = mid + 1
+        elif (arr[mid] == num):
+            return mid
+        else:
+            end = mid - 1
+
+    return -1
 
 def main():
 	arr = [1,2,4,6,8,9]
-	print(binSearch(arr, 10, 0, len(arr)-1))
+	print(binSearch(arr, 0, len(arr)-1), 10)
 
 main()
