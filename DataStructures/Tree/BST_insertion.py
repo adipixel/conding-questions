96ec0b43b9e9275f7861f6369dd28d34ea3f2351
Aditya Mhamunkar <me@adityamhamunkar.com>
2018-02-19 17:51:00 -0500

    Create BST_insertion.py

---
+++ DataStructures/Tree/BST_insertion.py
@@ -0,0 +1,23 @@
+"""
+Node is defined as
+self.left (the left child of the node)
+self.right (the right child of the node)
+self.data (the value of the node)"""
+
+def insert(r,val):
+    node = r
+    while True:
+        if node.data == val:
+            return r
+        if val < node.data:
+            if node.left is None:
+                node.left = Node(val)
+                return r
+            node = node.left
+        else:
+            if node.right is None:
+                node.right = Node(val)
+                return r
+            node = node.right
+        
+            
