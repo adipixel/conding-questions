2b89f4adc2dcd70c3426c55ee70d638bf97b4d5b
Aditya Mhamunkar <me@adityamhamunkar.com>
2018-05-05 22:42:51 -0400

    Create JS_prototypes.js

---
+++ JS_prototypes.js
@@ -0,0 +1,20 @@
+const Food = function(){};
+Food.prototype.eat = function(){
+	return('I ate food');
+}
+
+var Apple = function(){};
+
+Apple.prototype = new Food();
+
+Apple.prototype.taste = function(){
+  return('Apple is sweet')
+}
+
+Apple.prototype.eat = function(){
+  return('I ate an apple')
+}
+
+var apple = new Apple();
+console.log(apple.eat())
+console.log(apple.taste())
