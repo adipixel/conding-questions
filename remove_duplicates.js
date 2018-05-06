efd7c723466c4d240913fccef23ce43ee4b1315c
Aditya Mhamunkar <me@adityamhamunkar.com>
2018-05-06 04:13:07 -0400

    Create remove_duplicates.js

---
+++ remove_duplicates.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    var ptr = 1
+    var rpr = 1
+    while(ptr < nums.length){
+        
+        if(nums[ptr] == nums[ptr-1]){
+            while( ptr<nums.length && nums[ptr] == nums[ptr-1]){
+                ptr +=1
+            }
+            
+            nums[rpr]=nums[ptr]
+            ptr +=1
+            rpr +=1
+        }
+        else{
+            ptr +=1
+            rpr +=1
+        }
+    }
+    nums.length = rpr
+    //console.log(nums)
+    
+};
