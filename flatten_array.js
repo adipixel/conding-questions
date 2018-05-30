8a0a8bab8dd2f64ef4a57bde597e1c82bb51f1e8
Aditya Mhamunkar <me@adityamhamunkar.com>
2018-05-30 00:42:17 -0400

    Update flatten_array.js

--- flatten_array.js
+++ flatten_array.js
@@ -1,6 +1,6 @@
 var arr = [1,2,3,[1,2,3,4, [2,3,4]]];
 
-function flatten(arr){
+function flattenRecursive(arr){
 	var res = arr.reduce((acc, val)=>{
   	if(Array.isArray(val)){
     	return acc.concat(flatten(val))
@@ -13,5 +13,19 @@ function flatten(arr){
 	return res
 }
 
+function flattenIterative(arr) {
+  var array = [];
+  while(arr.length) {
+    var value = arr.shift();
+    if(Array.isArray(value)) {
+      // this line preserve the order
+      arr = value.concat(arr);
+    } else {
+      array.push(value);
+    }
+  }
+  return array;
+}
 
-console.log(flatten(arr))
+console.log(flattenRecursive(arr))
+console.log(flattenIterative(arr))
